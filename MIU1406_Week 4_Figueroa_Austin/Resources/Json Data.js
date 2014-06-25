3df8b826410ecd4006aa171c2a4c45b0761a1adc
Austin Figueroa <Austin@Austins-MacBook-Pro.local>
2014-06-25 18:55:59 -0400

    Starting off

---
+++ MIU1406_Week 4_Figueroa_Austin/Resources/Json Data.js
@@ -0,0 +1,26 @@
+var settings = {
+	"info":{
+		"Champion Names": "My Information",
+		"championData": [
+		    {
+				"title": "Audio Volume", 
+				"description":  "Adjust your Volume Here!",
+			},
+			 {
+				"title": "My Number", 
+				"description":  "Did you think we would let you forget that?",
+			},
+			 {
+				"title": "Bluetooth Settings", 
+				"description":  "Let's help you with some Bluetooth activity",
+			},
+			 {
+				"title": "Wi-Fi Connection", 
+				"description":  "Do you want Wi-Fi connected?",
+			},
+				
+		]
+	}
+};
+	
+	exports.settingData = settings;
